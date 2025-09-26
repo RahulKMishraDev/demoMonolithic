5450a81e5a597cb71e8f8c737320c1551e3a3341
RahulKMishraDev <rahulkumarmishra2003@gmail.com>
2025-09-26 19:48:24 +0530

    Initial commit

---
+++ src/main/java/com/example/demoMonolithic/config/LoggingAspectConfiguration.java
@@ -0,0 +1,17 @@
+package com.example.demoMonolithic.config;
+
+import com.example.demoMonolithic.aop.logging.LoggingAspect;
+import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
+import tech.jhipster.config.JHipsterConstants;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class LoggingAspectConfiguration {
+
+    @Bean
+    @Profile(JHipsterConstants.SPRING_PROFILE_DEVELOPMENT)
+    public LoggingAspect loggingAspect(Environment env) {
+        return new LoggingAspect(env);
+    }
+}
